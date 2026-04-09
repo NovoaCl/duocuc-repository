7984a7032718b3659d6c5836691e69ebbc848a53
NovoaCl <cl.novoa.ira@gmail.com>
2026-04-09 08:07:04 -0400

    Update FullStacl and DBii

--- 3rd/desarrollo-fullstack-i/EA1/projects/partysApp/src/main/java/cl/duoc/partysApp/controller/PartyController.java
+++ 3rd/desarrollo-fullstack-i/EA1/projects/partysApp/src/main/java/cl/duoc/partysApp/controller/PartyController.java
@@ -1,8 +1,14 @@
 package cl.duoc.partysApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import cl.duoc.partysApp.dto.response.PartyResponse;
+import cl.duoc.partysApp.service.PartyService;
 
 @RestController
 @RequestMapping("/api/v1/party")
@@ -10,4 +16,19 @@ public class PartyController {
     @Autowired
     private PartyService partyService;
 
+    //Get party Endpoint
+    @GetMapping("{id}")
+    public ResposeEntity<PartyResponse> searchById(@PathVariable Integer id) {
+        PartyResponse party = partyService.searchPartyById(id);
+        if (party == null) {
+            return ResponseEntity.notFound().build(); // Dvuelve el error 404 sin cuerpo o mensaje
+        }
+        return ResponseEntity.ok(party); // Devuelve 200 y su json
+    }
+
+    // Get all the parties
+    @GetMapping("parties")
+    public ResponseEntity<List<PartyResponse>> listAllParties() {
+        List<PartyResponse> parties = partyService.
+    }
 }
--- 3rd/desarrollo-fullstack-i/EA1/projects/partysApp/src/main/java/cl/duoc/partysApp/model/Party.java
+++ 3rd/desarrollo-fullstack-i/EA1/projects/partysApp/src/main/java/cl/duoc/partysApp/model/Party.java
@@ -2,20 +2,35 @@ package cl.duoc.partysApp.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "party")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Party {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Generates the id automatically, trigger
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String typeOfParty;
+
+    @Column(name = "relization_date", nullable = false)
     private LocalDate date;
+
+    @Column(nullable = false)
     private String location;
+
+    @Column(nullable = false)
     private Integer numberOfGuests;
 
 }
